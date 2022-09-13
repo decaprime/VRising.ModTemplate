d3e60027e0e14329903c460eb2afcc6c86ba1636
deca <git@deca.gg>
2022-09-13 14:32:47 -0700

    🎉 Initial commit of V Rising mod template

---
+++ templates/VRising.ModTemplate/Plugin.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+#if(UseVCF)
+using VampireCommandFramework;
+#endif
+
+namespace MOUTHWASH;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+#if(UseVCF)
+[BepInDependency("gg.deca.VampireCommandFramework")]
+#endif
+#if(UseWetstone)
+[BepInDependency("xyz.molenzwiebel.wetstone")]
+[Wetstone.API.Reloadable]
+#endif
+public class Plugin : BasePlugin
+{
+    Harmony _harmony;
+
+    public override void Load()
+    {
+        // Plugin startup logic
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} version {MyPluginInfo.PLUGIN_VERSION} is loaded!");
+
+        // Harmony patching
+        _harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+        _harmony.PatchAll(System.Reflection.Assembly.GetExecutingAssembly());
+
+#if (UseVCF)
+        // Register all commands in the assembly with VCF
+        CommandRegistry.RegisterAll();
+#endif
+    }
+
+    public override bool Unload()
+    {
+        _harmony?.UnpatchSelf();
+        return true;
+    }
+}
